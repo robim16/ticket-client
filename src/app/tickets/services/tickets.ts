0a511a12ab189e3ae4109d1c3759606f43fbb80a
robim16 <carteagajimenez@gmail.com>
2025-12-04 17:39:49 -0500

    cambios en edit ticket

--- src/app/tickets/services/tickets.ts
+++ src/app/tickets/services/tickets.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Ticket } from "../components/ticket-item/ticket-item";
+import { catchError } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class TicketsService {
@@ -11,18 +12,38 @@ export class TicketsService {
   constructor(private http: HttpClient) {}
 
   create(ticket: { description: string }) {
-    return this.http.post(`${this.baseUrl}`, ticket);
+    return this.http.post(`${this.baseUrl}`, ticket).pipe(
+      catchError((error) => {
+        console.error('Error creating a ticket:', error);
+        throw error;
+      })
+    );
   }
 
   findAll() {
-    return this.http.get(`${this.baseUrl}`);
+    return this.http.get(`${this.baseUrl}`).pipe(
+      catchError((error) => {
+        console.error('Error fetching tickets:', error);
+        throw error;
+      })
+    );
   }
 
   findOne(id: string): Observable<Ticket> {
-    return this.http.get<Ticket>(`${this.baseUrl}/${id}`);
+    return this.http.get<Ticket>(`${this.baseUrl}/${id}`).pipe(
+      catchError((error) => {
+        console.error('Error fetching a ticket:', error);
+        throw error;
+      })
+    );
   }
 
-  updateStatus(id: string, status: string) {
-    return this.http.patch(`${this.baseUrl}/${id}/status`, { status });
+  updateStatus(id: string, estado: string) {
+    return this.http.patch(`${this.baseUrl}/${id}/status`, { estado }).pipe(
+      catchError((error) => {
+        console.error('Error updating the ticket:', error);
+        throw error;
+      })
+    );
   }
 }
